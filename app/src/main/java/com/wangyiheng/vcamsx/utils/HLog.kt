316a3f325da5f92f6f4570a3a84add039987a1d0
0xD4nz <157308325+0xD4nz@users.noreply.github.com>
2025-06-17 06:06:41 +0700

    Update HLog.kt

--- app/src/main/java/com/wangyiheng/vcamsx/utils/HLog.kt
+++ app/src/main/java/com/wangyiheng/vcamsx/utils/HLog.kt
@@ -6,45 +6,49 @@ import de.robv.android.xposed.XposedBridge
 import java.io.*
 import java.util.*
 import com.bigkoo.pickerview.view.WheelTime.dateFormat
+
 object HLog {
-    var lastTransitionTime: Long = 0 // 初始化为0
+    var lastTransitionTime: Long = 0 // Initialize as 0
     val logBuffer = mutableListOf<String>()
     val MAX_LOG_ENTRIES = 5
-    fun d(logtype:String?="虚拟摄像头", msg: String) {
+
+    fun d(logtype: String? = "Virtual Camera", msg: String) {
         XposedBridge.log("$logtype:$msg")
     }
-    fun localeLog(context: Context,msg:String) {
+
+    fun localeLog(context: Context, msg: String) {
         val currentTimeMillis = System.currentTimeMillis()
         val formattedDate = dateFormat.format(Date(currentTimeMillis))
 
         val timeInterval = if (lastTransitionTime != 0L) {
-            (currentTimeMillis - lastTransitionTime)  // 将毫秒转换为秒
+            (currentTimeMillis - lastTransitionTime)  // Milliseconds between logs
         } else {
             0L
         }
-        // 更新上次切换时间
+        // Update last switch time
         lastTransitionTime = currentTimeMillis
-        val logMessage = "时间：$formattedDate\n$msg \n日志间隔时间：${timeInterval}毫秒"
-        Log.d("dbb",logMessage)
+        val logMessage = "Time: $formattedDate\n$msg \nInterval since last log: ${timeInterval} ms"
+        Log.d("dbb", logMessage)
 
-        // 将日志消息添加到缓冲区
+        // Add log message to buffer
         logBuffer.add(logMessage)
 
-        // 如果缓冲区中的日志条目达到二十条，则保存到文件并清空缓冲区
+        // If the buffer reaches max entries, save to file and clear buffer
         if (logBuffer.size >= MAX_LOG_ENTRIES) {
             saveLogsToFile(context)
         }
     }
+
     private fun saveLogsToFile(context: Context) {
         val logFileDir = context.getExternalFilesDir(null)!!.absolutePath
         val logFilePath = File(logFileDir, "log.txt")
 
         try {
-            // 将缓冲区中的日志消息写入文件
+            // Write buffered log messages to file
             logBuffer.forEach { logMessage ->
                 logFilePath.appendText(logMessage + "\n\n")
             }
-            // 清空缓冲区
+            // Clear buffer
             logBuffer.clear()
         } catch (e: IOException) {
             e.printStackTrace()
